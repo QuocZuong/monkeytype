858d9a9997fb90ba183da72407159fdd8a075c6f
TrucVy <vyce171243.fpt.vn>
2023-10-13 18:06:06 +0700

    Create Footer component

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -3,7 +3,6 @@ import classNames from "classnames/bind";
 import { Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    // faBell,
     faCode,
     faCodeBranch,
     faDonate,
@@ -12,77 +11,124 @@ import {
     faLock,
     faPallet,
     faShieldAlt,
-    // faUser,
 } from "@fortawesome/free-solid-svg-icons";
 
 import styles from "./Footer.module.scss";
+import { faDiscord, faTwitter } from "@fortawesome/free-brands-svg-icons";
 
 const cx = classNames.bind(styles);
 const Footer = () => {
     return (
         <Container fluid className={cx("wrapper")}>
             <Row className={cx("row")}>
-                <Col className={cx("left")}>
-                    <div className={cx("col-wrapper")}>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faEnvelope} size="lg" /> Contact
-                            </i>
-                        </button>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faEnvelope} size="lg" /> Twitter
-                            </i>
-                        </button>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faDonate} size="lg" /> Support
-                            </i>
-                        </button>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faFileContract} size="lg" /> Term
-                            </i>
-                        </button>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faCode} size="lg" /> GitHub
-                            </i>
-                        </button>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faShieldAlt} size="lg" /> Security
-                            </i>
-                        </button>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faDiscord} style={{ color: "#000000" }} />
-                                Discord
-                            </i>
-                        </button>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faLock} size="lg" /> Privatcy
-                            </i>
-                        </button>
+                <div className={cx("footer-wrapper")}>
+                    <div className={cx("key-tips")}>
+                        <>
+                            <div className={cx("key-tips1")}>
+                                <div className={cx("key-text")}>tab</div> + <div className={cx("key-text")}>enter</div>{" "}
+                                - restart test
+                            </div>
+
+                            <div className={cx("key-tips2")}>
+                                <div className={cx("key-text")}>esc</div> or <div className={cx("key-text")}>ctrl</div>{" "}
+                                + <div className={cx("key-text")}>shift</div> + <div className={cx("key-text")}>p</div>{" "}
+                                - command line
+                            </div>
+                        </>
                     </div>
-                </Col>
-                <Col className={cx("right")}>
-                    <div className={cx("col-wrapper")}>
-                        <button title="leaderboard" className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faPallet} size="lg" />
-                                serika dark
-                            </i>
-                        </button>
-                        <button className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faCodeBranch} size="lg" />
-                                v23.40.1
-                            </i>
-                        </button>
+                    <div className={cx("leftright")}>
+                        <Col className={cx("left")}>
+                            <div className={cx("col-wrapper")}>
+                                <button title="leaderboard" className={cx("link")}>
+                                    <i>
+                                        <FontAwesomeIcon icon={faEnvelope} size="lg" style={{ marginRight: "7px" }} />
+                                        Contact
+                                    </i>
+                                </button>
+                                <button title="leaderboard" className={cx("link")}>
+                                    <i>
+                                        <FontAwesomeIcon icon={faDonate} size="lg" style={{ marginRight: "7px" }} />{" "}
+                                        Support
+                                    </i>
+                                </button>
+                                <a
+                                    href="https://github.com/monkeytypegame/monkeytype"
+                                    className={cx("link")}
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                >
+                                    <i>
+                                        <FontAwesomeIcon icon={faCode} size="lg" style={{ marginRight: "7px" }} />{" "}
+                                        GitHub
+                                    </i>
+                                </a>
+
+                                <a
+                                    href="https://www.discord.gg/monkeytype"
+                                    className={cx("link")}
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                >
+                                    <i>
+                                        <FontAwesomeIcon icon={faDiscord} size="lg" style={{ marginRight: "7px" }} />{" "}
+                                        Discord
+                                    </i>
+                                </a>
+
+                                <a
+                                    href="https://twitter.com/monkeytypegame"
+                                    className={cx("link")}
+                                    target="_blank"
+                                    rel="noreferrer noopener"
+                                >
+                                    <i>
+                                        <FontAwesomeIcon icon={faTwitter} size="lg" style={{ marginRight: "7px" }} />{" "}
+                                        Twitter
+                                    </i>
+                                </a>
+
+                                <button title="leaderboard" className={cx("link")}>
+                                    <i>
+                                        <FontAwesomeIcon
+                                            icon={faFileContract}
+                                            size="lg"
+                                            style={{ marginRight: "7px" }}
+                                        />{" "}
+                                        Term
+                                    </i>
+                                </button>
+                                <button title="leaderboard" className={cx("link")}>
+                                    <i>
+                                        <FontAwesomeIcon icon={faShieldAlt} size="lg" style={{ marginRight: "7px" }} />{" "}
+                                        Security
+                                    </i>
+                                </button>
+                                <button title="leaderboard" className={cx("link")}>
+                                    <i>
+                                        <FontAwesomeIcon icon={faLock} size="lg" style={{ marginRight: "7px" }} />{" "}
+                                        Privatcy
+                                    </i>
+                                </button>
+                            </div>
+                        </Col>
+                        <Col className={cx("right")}>
+                            <div className={cx("col-wrapper")}>
+                                <button title="leaderboard" className={cx("link")}>
+                                    <i>
+                                        <FontAwesomeIcon icon={faPallet} size="lg" style={{ marginRight: "7px" }} />
+                                        serika dark
+                                    </i>
+                                </button>
+                                <button className={cx("link")}>
+                                    <i>
+                                        <FontAwesomeIcon icon={faCodeBranch} size="lg" style={{ marginRight: "7px" }} />
+                                        v23.40.1
+                                    </i>
+                                </button>
+                            </div>
+                        </Col>
                     </div>
-                </Col>
+                </div>
             </Row>
         </Container>
     );
