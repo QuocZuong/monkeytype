0f47ad7d3ed4425ba59169f02531d00b33633f66
Truc Vy <vymntce171243@fpt.edu.vn>
2023-10-19 18:57:58 +0700

    Responsive for typing section

--- frontend/src/layouts/MainLayout/MainLayout.tsx
+++ frontend/src/layouts/MainLayout/MainLayout.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import classNames from "classnames/bind";
 
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import styles from "./MainLayout.module.scss";
 
 import { ComponentProps } from "@/Models/ComponentProps";
-import Footer from "@/components/Footer";
+// import Footer from "@/components/Footer";
 
 const cx = classNames.bind(styles);
 const MainLayout: React.FC<ComponentProps> = ({ children }) => {
