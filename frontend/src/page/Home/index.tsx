5510aaf889c81d0a26ff3071dc79fa155e6a2477
Zuong <quocvuongle.ct@gmail.com>
2023-11-06 14:52:27 +0700

    update responsive

--- frontend/src/page/Home/index.tsx
+++ frontend/src/page/Home/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import TypingTest from "@/components/TypingTest";
 const Home = () => {
     return (
-        <div className="home-page">
+        <div className="home-page d-flex justify-content-center w-100">
             <TypingTest />
         </div>
     );
