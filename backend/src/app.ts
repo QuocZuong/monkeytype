d3ff210c49218ca5d812330ca67c7650c31facbe
phenchua29 <phenchua99122@gmail.com>
2023-11-06 17:16:42 +0700

    Use new routing system

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,23 +1,11 @@
-import { Request, Response } from "express";
-import express from "express"
-
-import uDao from "./DAO/UserDao";
-import User from "./Model/User";
+import express from "express";
+import Routes from "./Routes/Routes";
 
 const app = express();
-// const uDao = new uDao();
+
+Routes.forEach((route) => {
+  app.use(route.path, route.router);
+  console.log(`Added route for path ${route.path}`);
+});
 
 app.listen(50000);
-
-// app.use('/', routes)
-
-app.get("/", async (req: Request, res: Response) => {
-  console.log("Got a request on /");
-
-  // await uDao.createUser(new User("Heo", "HeoMap"));
-  let result = await uDao.getAllUser();
-
-  console.log(`result from query: ${result}`);
-
-  res.json(result);
-});
