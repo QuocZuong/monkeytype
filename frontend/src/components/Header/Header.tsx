41832c5ce17d18caf69f8f446ee85b9c2db2221f
phenchua29 <phenchua99122@gmail.com>
2023-11-06 20:17:17 +0700

    Refactor

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import classNames from "classnames/bind";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell, faCrown, faGear, faInfo, faKeyboard, faSignOut, faUser } from "@fortawesome/free-solid-svg-icons";
@@ -8,17 +8,83 @@ import { faBell, faCrown, faGear, faInfo, faKeyboard, faSignOut, faUser } from "
 import config from "@/config";
 import images from "@/assets/images";
 import styles from "./Header.module.scss";
+import { setGlobalState, useGlobalState } from "@/globalState";
 
 const cx = classNames.bind(styles);
 
 /**
- * The header component
+ * The user section component.
+ * @returns {ReactNode} return a user section component.
+ */
+const UserSection = () => {
+    const [isLoggedIn] = useGlobalState("isLoggedIn");
+    const [user] = useGlobalState("user");
+    const nagivator = useNavigate();
+    const level = 1;
+
+
+    const handleSignout = () => {
+        alert("Signing out");
+        setGlobalState("isLoggedIn", false);
+        nagivator(config.routeLinks.home);
+    };
+
+    if (isLoggedIn) {
+        return (
+            <>
+                <Col md={2}>
+                    <Link to={config.routeLinks.profile} className={cx("user-info")}>
+                        <i>
+                            <FontAwesomeIcon icon={faUser} size="lg" />
+                        </i>
+                        <div className={cx("username")}>{user.username}</div>
+                        <div className={cx("level")}>{level}</div>
+                    </Link>
+                </Col>
+                <Col md={1}>
+                    <button className={cx("link")}>
+                        <i>
+                            <FontAwesomeIcon icon={faBell} size="lg" />
+                        </i>
+                    </button>
+                </Col>
+                <Col md={1}>
+                    <button className={cx("link")} onClick={() => handleSignout()}>
+                        <i>
+                            <FontAwesomeIcon icon={faSignOut} size="lg" />
+                        </i>
+                    </button>
+                </Col>
+            </>
+        );
+    }
+
+    return (
+        <>
+            <Col md={1}>
+                <button className={cx("link")}>
+                    <i>
+                        <FontAwesomeIcon icon={faBell} size="lg" />
+                    </i>
+                </button>
+            </Col>
+            <Col md={1}>
+                <Link to={config.routeLinks.login} className={cx("user-info")}>
+                    <i>
+                        <FontAwesomeIcon icon={faUser} size="lg" />
+                    </i>
+                </Link>
+            </Col>
+        </>
+    );
+};
+
+/**
+ * The header component.
  *
- * @returns {ReactNode} return a header component apply for almost all pages
+ * @returns {ReactNode} return a header component apply for almost all pages.
  */
 const Header = () => {
-    const isLogged = true;
-
     return (
         <Container fluid className={cx("wrapper")}>
             <Row className={cx("row")}>
@@ -51,33 +117,7 @@ const Header = () => {
                 </Col>
                 <Col className={cx("right")}>
                     <div className={cx("col-wrapper")}>
-                        <Link to={config.routeLinks.profile} className={cx("user-info")}>
-                            <i>
-                                <FontAwesomeIcon icon={faUser} size="lg" />
-                            </i>
-
-                            {/* Check if user logged or not to show icon*/}
-                            {isLogged && (
-                                <>
-                                    <div className={cx("username")}>zuong</div>
-                                    <div className={cx("level")}>111</div>
-                                </>
-                            )}
-                        </Link>
-                        <button className={cx("link")}>
-                            <i>
-                                <FontAwesomeIcon icon={faBell} size="lg" />
-                            </i>
-                        </button>
-
-                        {/* Check if user logged or not to show icon*/}
-                        {isLogged && (
-                            <button className={cx("link")}>
-                                <i>
-                                    <FontAwesomeIcon icon={faSignOut} size="lg" />
-                                </i>
-                            </button>
-                        )}
+                        <UserSection />
                     </div>
                 </Col>
             </Row>
