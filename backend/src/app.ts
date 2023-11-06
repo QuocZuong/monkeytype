259c9f79190b4f48aa9e749635ae3e234600787f
phenchua29 <phenchua99122@gmail.com>
2023-11-06 20:10:14 +0700

    Allowing cross origin requests, enabling POST and PUT json body parser

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
+import cors from "cors";
 import Routes from "./Routes/Routes";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 Routes.forEach((route) => {
   app.use(route.path, route.router);
